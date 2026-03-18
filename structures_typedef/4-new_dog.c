32aa358f686c722ae25ff6ed8d62f75b6a580926
udcyber <udewaleyne.pro@gmail.com>
2026-03-18 08:12:47 +0100

    add header, create new dog

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -24,15 +24,17 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	dog2 = malloc(sizeof(dog_t));
 	if (dog2 == 0)
+	{
 		free(dog2);
 		return (NULL);
-
+	}
 	dog2->name = malloc(sizeof(dog2->name) * a);
 	if (dog2->name == 0)
+	{
 		free(dog2->name);
 		free(dog2);
 		return (NULL);
-
+	}
 	for (len = 0; len < a; len++)
 		dog2->name[len] = name[len];
 	dog2->name[len] = name[len];
@@ -41,11 +43,12 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	dog2->owner = malloc(sizeof(dog2->owner) * b);
 	if (dog2->owner == 0)
+	{
 		free(dog2->owner);
 		free(dog2->name);
 		free(dog2);
 		return (NULL);
-
+	}
 	for (len = 0; len < b; len++)
 		dog2->owner[len] = owner[len];
 	dog2->owner[len] = owner[len];
